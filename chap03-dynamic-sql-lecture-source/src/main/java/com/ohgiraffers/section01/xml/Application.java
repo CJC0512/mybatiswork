a25f96d0a0c3b83bd6d91d0c733c53d776b5400d
CJC0512 <ehdchs2006@gmail.com>
2024-02-21 12:40:21 +0900

    chap03 chooseSubMenu, foreachSubMenu 구현

--- chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
+++ chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
@@ -1,6 +1,6 @@
 package com.ohgiraffers.section01.xml;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -15,13 +15,15 @@ public class Application {
             System.out.print("메뉴를 선택하세요: ");
             int no = sc.nextInt();
 
-            switch (no){
+            switch (no) {
                 case 1:
                     ifSubMenu();
                     break;
                 case 2:
+                    chooseSubMenu();
                     break;
                 case 3:
+                    foreachSubMenu();
                     break;
                 case 4:
                     break;
@@ -29,13 +31,14 @@ public class Application {
                     System.out.println("프로그램을 종료합니다.");
                     return;
             }
-        } while(true);
+        } while (true);
     }
+
 
     private static void ifSubMenu() {
         Scanner sc = new Scanner(System.in);
         MenuService menuService = new MenuService();
-        do{
+        do {
             System.out.println("====== if 서브 메뉴 ======");
             System.out.println("1. 원하는 금액대에 적합한 추천 메뉴 목록 보여주기");
             System.out.println("2. 메뉴 이름 혹은 카테고리명으로 검색하여 메뉴 목록 보여주기");
@@ -43,7 +46,7 @@ public class Application {
             System.out.print("메뉴 번호를 입력해 주세요: ");
             int no = sc.nextInt();
 
-            switch (no){
+            switch (no) {
                 case 1:
                     menuService.findMenuByPrice(inputPrice());
                     break;
@@ -53,7 +56,7 @@ public class Application {
                 case 9:
                     return;
             }
-        }while(true);
+        } while (true);
     }
 
     private static int inputPrice() {
@@ -71,5 +74,72 @@ public class Application {
         String value = sc.nextLine();
 
         return new SearchCriteria(condition, value);
+    }
+
+    private static void chooseSubMenu() {
+        Scanner sc = new Scanner(System.in);
+        MenuService menuService = new MenuService();
+
+        do {
+            System.out.println("====== choose 서브 메뉴 ======");
+            System.out.println("1. 카테고리 상위 분류별 메뉴 보여주기(식사, 음료, 디저트)");
+            System.out.println("9. 이전 메뉴로");
+            System.out.print("메뉴 번호를 입력하세요: ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1:
+                    menuService.searchMenuBySupCategory(inputSupCategory());
+                    break;
+                case 9:
+                    return;
+            }
+        } while (true);
+    }
+
+    private static SearchCriteria inputSupCategory() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("상위 분류를 입력해 주세요(식사, 음료, 디저트): ");
+        String value = sc.nextLine();
+
+        return new SearchCriteria("category", value);
+    }
+
+    private static void foreachSubMenu() {
+        Scanner sc = new Scanner(System.in);
+        MenuService menuService = new MenuService();
+
+        do {
+            System.out.println("====== foreach 서브 메뉴 ======");
+            System.out.println("1. 랜덤한 메뉴 5개 추출해서 조회하기");
+            System.out.println("9. 이전 메뉴로");
+            System.out.print("메뉴 번호를 입력하세요: ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1:
+                    menuService.searchMenuByRandomMenuCode(generateRandomMenuCodeList());
+                    break;
+                case 9:
+                    return;
+            }
+        } while (true);
+    }
+
+    /* 설명. 1부터 21까지의 5개의 중복되지 않는 번호 생성해 List에 쌓아 반환하는 메소드 */
+    private static List<Integer> generateRandomMenuCodeList() {
+        Set<Integer> set = new HashSet<>();
+        while (set.size() < 5) {
+            int temp = ((int) (Math.random() * 21) + 1);
+            set.add(temp);
+        }
+
+        /* 설명. set을 List형태로 바꿔서 오름차순 정렬 후 반환 */
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        System.out.println("랜덤 수 생성한 메소드가 반환하는 list: " + list);
+
+        return list;
     }
 }
--- chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
+++ chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
@@ -9,12 +9,14 @@ import static com.ohgiraffers.section01.xml.Template.getSqlSession;
 public class MenuService {
 
     public void findMenuByPrice(int maxPrice) {
+
         SqlSession sqlSession = getSqlSession();
         MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
 
         List<MenuDTO> menus = mapper.selectMenuByPrice(maxPrice);
         System.out.println("[Service]");
-        menus.forEach(System.out :: println);
+        menus.forEach(System.out::println);
+        sqlSession.close();
     }
 
     public void searchMenu(SearchCriteria searchCriteria) {
@@ -25,5 +27,37 @@ public class MenuService {
         List<MenuDTO> menus = mapper.searchMenu(searchCriteria);
         System.out.println("[Service]");
         menus.forEach(System.out::println);
+        sqlSession.close();
+    }
+
+    public void searchMenuBySupCategory(SearchCriteria searchCriteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
+
+        List<MenuDTO> menus = mapper.searchMenuBySupCategory(searchCriteria);
+
+        if (menus != null && menus.size() > 0) {
+            menus.forEach(System.out::println);
+        } else {
+            System.out.println("DB와 연동 실패 또는 검색 결과 없음");
+        }
+        sqlSession.close();
+    }
+
+    public void searchMenuByRandomMenuCode(List randomList) {
+
+        SqlSession sqlSession = getSqlSession();
+        MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
+
+        List<MenuDTO> menus = mapper.searchMenuByRandomMenuCode(randomList);
+
+        if (menus != null && menus.size() > 0) {
+            menus.forEach(System.out::println);
+        } else {
+            System.out.println("DB와 연동 실패 또는 검색 결과 없음");
+        }
+
+        sqlSession.close();
     }
 }
