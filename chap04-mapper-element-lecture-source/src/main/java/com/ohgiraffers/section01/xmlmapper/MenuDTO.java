063cc591af853725427219be8358ca075d35f3e1
CJC0512 <ehdchs2006@gmail.com>
2024-02-22 11:16:25 +0900

    chap04 section01
    requestMap, association, collection test

---
+++ chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/MenuDTO.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+public class MenuDTO {
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private int categoryCode;
+    private String orderableStatus;
+
+    public MenuDTO() {
+    }
+
+    public MenuDTO(int menuCode, String menuName, int menuPrice, int categoryCode, String orderableStatus) {
+        this.menuCode = menuCode;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.categoryCode = categoryCode;
+        this.orderableStatus = orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuDTO{" +
+                "menuCode=" + menuCode +
+                ", menuName='" + menuName + '\'' +
+                ", menuPrice=" + menuPrice +
+                ", categoryCode=" + categoryCode +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+}
